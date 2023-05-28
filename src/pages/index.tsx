40167ccb86d0504c9179d8bf355fd9fbf6953ba0
Joy <mmehedihasanjoyv@gmail.com>
2023-05-28 10:09:27 +0600

    done the basic setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main>
+      <h2 className='text-xl'>Hello world</h2>
+ </main>
+  )
+}
